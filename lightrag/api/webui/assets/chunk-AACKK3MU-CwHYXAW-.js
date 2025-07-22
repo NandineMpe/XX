0e6c10bac0f9534e84771701d4ac93af6ff6ca7c
NandineMpe <nandinempe@gmail.com>
2025-07-22 16:58:31 +0100

    chore: update dashboard UI (automated script)

--- lightrag/api/webui/assets/chunk-AACKK3MU-CwHYXAW-.js
+++
@@ -1,1 +0,0 @@
-import{_ as s}from"./mermaid-vendor-DYf4I_wf.js";var t,e=(t=class{constructor(i){this.init=i,this.records=this.init()}reset(){this.records=this.init()}},s(t,"ImperativeState"),t);export{e as I};
--- lightrag/api/webui/assets/clone-BjXOT6z-.js
+++
@@ -1,1 +0,0 @@
-import{b as r}from"./_baseUniq-Crxsc_jV.js";var e=4;function a(o){return r(o,e)}export{a as c};
