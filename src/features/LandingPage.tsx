041f4e3060dfde332a37bba0d5c1ce90bdb2ff2c
NandineMpe <nandinempe@gmail.com>
2025-07-25 13:34:56 +0100

    Fix scrolling issues by removing flex constraints and ensuring proper overflow settings

--- src/features/LandingPage.tsx
+++ src/features/LandingPage.tsx
@@ -113,7 +113,7 @@ const LandingPage = () => {
   ]
 
   return (
-    <div className="min-h-screen bg-black text-white flex flex-col items-center justify-start">
+    <div className="min-h-screen bg-black text-white">
       {/* Tubelight Navbar */}
       <TubelightNavBar items={navItems} logoUrl={logoUrl} />
 
