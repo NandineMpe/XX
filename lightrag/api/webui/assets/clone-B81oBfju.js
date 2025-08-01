deeba7d37826e7bfdc2fa0d40677b4a938e48466
NandineMpe <nandinempe@gmail.com>
2025-08-01 21:08:27 +0100

    chore: update dashboard UI (automated script)

---
+++ lightrag/api/webui/assets/clone-B81oBfju.js
@@ -0,0 +1,1 @@
+import{b as r}from"./_baseUniq-BpS6yoWH.js";var e=4;function a(o){return r(o,e)}export{a as c};
--- lightrag/api/webui/assets/clone-Def5hOlX.js
+++
@@ -1,1 +0,0 @@
-import{b as r}from"./_baseUniq-Dt4B1o1e.js";var e=4;function a(o){return r(o,e)}export{a as c};
